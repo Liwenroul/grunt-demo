52b9f27c9a63f500fa0d2025a62c21bc9f9321ee
Liwenroul <2728608191@qq.com>
2020-03-25 00:04:29 +0800

    http-api-test

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,24 @@
 module.exports=function(grunt){
+    
     grunt.initConfig({
-        mocha:{
-            test:{
-                src:["test/index.html"]
-            },
+        run:{
+            api:{
+                options:{wait:false},
+                arge:['./app.js']
+            }
+        },
+        mochacli:{
+            all:['test/*.js'],
             options:{
-                run:true,
-                reportor:'Dot'
+                reportor:'spec',
+                bail:true
             }
         }
     });
 
-    grunt.loadNpmTasks('grunt-mocha');
+    grunt.loadNpmTasks('grunt-mocha-cli');
+    grunt.loadNpmTasks('grunt-run');
 
-    grunt.registerTask('default',['mocha']);
+
+    grunt.registerTask('default',['run','mochacli','stop:api']);
 };
--- test/util.test.js
+++
@@ -1,9 +0,0 @@
-var expect= chai.expert;
-
-describe('矩形面积函数的测试',function(){
-	it('area(10,5)=50',function(){
-		var r=rectangle();
-		expect(r,area(10,5)).to.be.equal(50);
-	};
-
-});
