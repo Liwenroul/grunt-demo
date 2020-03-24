0376f70639af27ba7d2ec3c7f28d3bee2e7a1d76
Liwenroul <2728608191@qq.com>
2020-03-25 00:28:40 +0800

    htmlmin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,21 @@
 module.exports=function(grunt){
     
     grunt.initConfig({
-        run:{
-            api:{
-                options:{wait:false},
-                arge:['./app.js']
-            }
-        },
-        mochacli:{
-            all:['test/*.js'],
+        htmlmin:{
             options:{
-                reportor:'spec',
-                bail:true
+                removeComments:true,
+                collapseWhitespace:true
+            },
+            files:{
+                src:'./index.html',
+                dest:'dist/index.html'
             }
         }
     });
 
-    grunt.loadNpmTasks('grunt-mocha-cli');
-    grunt.loadNpmTasks('grunt-run');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    
 
 
-    grunt.registerTask('default',['run','mochacli','stop:api']);
+    grunt.registerTask('default',['htmlmin']);
 };
