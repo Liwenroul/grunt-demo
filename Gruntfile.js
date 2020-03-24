ec3af6f7f95c9cfb5c13482bc8904953c9f16aed
Liwenroul <2728608191@qq.com>
2020-03-25 00:39:25 +0800

    cssmin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,14 @@
 module.exports=function(grunt){
     
     grunt.initConfig({
-        htmlmin:{
-            options:{
-                removeComments:true,
-                collapseWhitespace:true
-            },
-            files:{
-                src:'./index.html',
-                dest:'dist/index.html'
-            }
+        cssmin:{
+            'a.min.css':'a.css'
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
     
 
 
-    grunt.registerTask('default',['htmlmin']);
+    grunt.registerTask('default',['cssmin']);
 };
